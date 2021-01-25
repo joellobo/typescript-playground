93fc830a137fc135cb97e30369be903b54ef6619
Joel Lobo <joellobo@gmail.com>
2021-01-25 10:43:24 -0300

    First commit

---
+++ index2.js
@@ -0,0 +1,4 @@
+var myName1;
+console.log("myName1:" + myName1);
+var myName2, string;
+console.log("myName2:" + myName2);
---
+++ index2.ts
@@ -0,0 +1,5 @@
+let myName1: string
+console.log("myName1:" + myName1)
+
+let myName2?: string
+console.log("myName2:" + myName2)
